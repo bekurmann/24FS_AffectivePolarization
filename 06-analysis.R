6aeb36d2d68df0caf263caf20d63b167cfadb89e
Benjamin Kurmann <benjamin@pop-os.localdomain>
2024-08-21 17:33:39 +0200

    seperated sotomo from non-sotomo analysis

--- 06-analysis.R
+++ 06-analysis.R
@@ -25,65 +25,52 @@ saveRDS(motions_polarization, "data/motions_polarization_with_lr_ap_inverted_con
 
 # # #################################################################################
 # testing data with sotomo
+# # #################################################################################
 
-# lm1 <- lm(motions_polarization$yes_votes ~ motions_polarization$lr_distance_normalized)
-# summary(lm1)
-# 
-# motions_polarization %>%
-#   ggplot(aes(x = lr_distance_normalized, y = yes_votes, color = passed)) +
-#   geom_point(alpha = 0.7, size = 3) +
-#   geom_smooth(method = "lm", se = TRUE,
-#               aes(group = 1),
-#               color = "black") +
-#   theme_minimal() +
-#   labs(
-#     x = "Normalized LR Distance",
-#     y = "'Yes' votes",
-#     title = "Yes Votes ~ Normalized LR Distance",
-#     color = "failed/passed"
-#   )
-# 
-# lm2 <- lm(motions_polarization$yes_votes ~ motions_polarization$ap_score_normalized)
-# summary(lm2)
-# 
-# motions_polarization %>%
-#   ggplot(aes(x = ap_score_normalized, y = yes_votes, color = passed)) +
-#   geom_point(alpha = 0.7, size = 3) +
-#   geom_smooth(method = "lm", se = TRUE,
-#               aes(group = 1),
-#               color = "black") +
-#   theme_minimal() +
-#   labs(
-#     x = "Normalized AP Score",
-#     y = "'Yes' votes",
-#     title = "Yes Votes ~ Normalized AP Acore",
-#     color = "failed/passed"
-#   )
-# 
-# 
-# lm3 <- lm(motions_polarization$yes_votes ~ 
-#             motions_polarization$ap_score_normalized + 
-#             motions_polarization$lr_distance_normalized +
-#             motions_polarization$FederalCouncilProposalEncoded +
-#             motions_polarization$multiple_councils)
-# summary(lm3)
-# 
-# motions_polarization %>%
-#   ggplot(aes(x = lr_distance_normalized, y = ap_score_normalized, color = passed)) +
-#   geom_point(alpha = 0.7, size = 3) +
-#   geom_smooth(method = "lm", se = TRUE,
-#               aes(group = 1),
-#               color = "black") +
-#   theme_minimal() +
-#   labs(
-#     x = "Normalized L/R Distance",
-#     y = "Normalized AP Score",
-#     title = "Normalized AP Score ~ Normalized L/R Distance",
-#     color = "failed/passed"
-#   )
+lm1_sotomo <- lm(motions_polarization$yes_votes ~ motions_polarization$lr_distance_normalized)
+summary(lm1_sotomo)
+
+lm2_sotomo <- lm(motions_polarization$yes_votes ~ motions_polarization$ap_score_normalized)
+summary(lm2_sotomo)
+
+lm3_sotomo <- lm(motions_polarization$yes_votes ~
+                   motions_polarization$ap_score_normalized +
+                   motions_polarization$lr_distance_normalized +
+                   motions_polarization$FederalCouncilProposalEncoded +
+                   motions_polarization$multiple_councils)
+summary(lm3_sotomo)
+
+motions_polarization %>%
+  ggplot(aes(x = ap_score_normalized, y = yes_votes, color = passed)) +
+  geom_point(alpha = 0.7, size = 3) +
+  geom_smooth(method = "lm", se = TRUE,
+              aes(group = 1),
+              color = "black") +
+  theme_minimal() +
+  labs(
+    x = "Normalized AP Score",
+    y = "'Yes' votes",
+    title = "Yes Votes ~ Normalized AP Acore",
+    color = "failed/passed"
+  )
+
+motions_polarization %>%
+  ggplot(aes(x = lr_distance_normalized, y = ap_score_normalized, color = passed)) +
+  geom_point(alpha = 0.7, size = 3) +
+  geom_smooth(method = "lm", se = TRUE,
+              aes(group = 1),
+              color = "black") +
+  theme_minimal() +
+  labs(
+    x = "Normalized L/R Distance",
+    y = "Normalized AP Score",
+    title = "Normalized AP Score ~ Normalized L/R Distance",
+    color = "failed/passed"
+  )
 
 # # #################################################################################
 # testing data without sotomo
+# # #################################################################################
 
 lm1 <- lm(motions_polarization$yes_votes ~ motions_polarization$lr_distance_normalized)
 summary(lm1)
@@ -112,7 +99,19 @@ motions_polarization %>%
     color = "failed/passed"
   )
 
-
+motions_polarization %>%
+  ggplot(aes(x = lr_distance_normalized, y = ap_score_normalized2, color = passed)) +
+  geom_point(alpha = 0.7, size = 3) +
+  geom_smooth(method = "lm", se = TRUE,
+              aes(group = 1),
+              color = "black") +
+  theme_minimal() +
+  labs(
+    x = "Normalized L/R Distance",
+    y = "Normalized AP Score",
+    title = "Normalized AP Score ~ Normalized L/R Distance",
+    color = "failed/passed"
+  )
 
 
 
@@ -316,3 +315,4 @@ lm_agri2 <- lm(motions_polarization_agri$yes_votes ~
                    motions_polarization_agri$multiple_councils)
 
 summary(lm_agri2)
+
