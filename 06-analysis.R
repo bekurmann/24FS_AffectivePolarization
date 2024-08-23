5439b46422e551d104e55a008261cea5718ed159
Benjamin Kurmann <benjamin@pop-os.localdomain>
2024-08-23 11:20:10 +0200

    cleaned up analysis script, added img export + figures

--- 06-analysis.R
+++ 06-analysis.R
@@ -4,69 +4,32 @@ library(tidyverse)
 motions_polarization <- readRDS("data/motions_polarization_with_lr_ap_inverted.rds")
 
 # # #################################################################################
-# summary
-
-summary(motions_polarization)
-
-# # #################################################################################
 # control variable FederalCouncilProposalText
 
 unique_values <- unique(motions_polarization$FederalCouncilProposalText)
 print(unique_values)
 
-# Assuming 'your_dataframe' is your dataframe and 'FederalCouncilProposalText' is your column
+# Encoding 'FederalCouncilProposalText' to 0/1
 motions_polarization$FederalCouncilProposalEncoded <- ifelse(
-  motions_polarization$FederalCouncilProposalText == "Der Bundesrat beantragt die Annahme der Motion.", 1, 0)
+  motions_polarization$FederalCouncilProposalText == "Der Bundesrat beantragt die Annahme der Motion." | 
+    motions_polarization$FederalCouncilProposalText == "Das Büro beantragt die Ablehnung der Motion.", TRUE, FALSE)
 
 # # #################################################################################
 # resave for assumptions
 saveRDS(motions_polarization, "data/motions_polarization_with_lr_ap_inverted_controls.rds")
 
-
 # # #################################################################################
-# testing data with sotomo
+# variable summaries
 # # #################################################################################
+# with sotomo
+summary(motions_polarization$yes_votes)
+summary(motions_polarization$lr_distance_normalized)
+summary(motions_polarization$ap_score_normalized)
+summary(motions_polarization$FederalCouncilProposalEncoded)
+summary(motions_polarization$multiple_councils)
 
-lm1_sotomo <- lm(motions_polarization$yes_votes ~ motions_polarization$lr_distance_normalized)
-summary(lm1_sotomo)
-
-lm2_sotomo <- lm(motions_polarization$yes_votes ~ motions_polarization$ap_score_normalized)
-summary(lm2_sotomo)
-
-lm3_sotomo <- lm(motions_polarization$yes_votes ~
-                   motions_polarization$ap_score_normalized +
-                   motions_polarization$lr_distance_normalized +
-                   motions_polarization$FederalCouncilProposalEncoded +
-                   motions_polarization$multiple_councils)
-summary(lm3_sotomo)
-
-motions_polarization %>%
-  ggplot(aes(x = ap_score_normalized, y = yes_votes, color = passed)) +
-  geom_point(alpha = 0.7, size = 3) +
-  geom_smooth(method = "lm", se = TRUE,
-              aes(group = 1),
-              color = "black") +
-  theme_minimal() +
-  labs(
-    x = "Normalized AP Score",
-    y = "'Yes' votes",
-    title = "Yes Votes ~ Normalized AP Acore",
-    color = "failed/passed"
-  )
-
-motions_polarization %>%
-  ggplot(aes(x = lr_distance_normalized, y = ap_score_normalized, color = passed)) +
-  geom_point(alpha = 0.7, size = 3) +
-  geom_smooth(method = "lm", se = TRUE,
-              aes(group = 1),
-              color = "black") +
-  theme_minimal() +
-  labs(
-    x = "Normalized L/R Distance",
-    y = "Normalized AP Score",
-    title = "Normalized AP Score ~ Normalized L/R Distance",
-    color = "failed/passed"
-  )
+# without sotomo
+summary(motions_polarization$ap_score_normalized2)
 
 # # #################################################################################
 # testing data without sotomo
@@ -85,7 +48,24 @@ lm3 <- lm(motions_polarization$yes_votes ~
             motions_polarization$multiple_councils)
 summary(lm3)
 
-motions_polarization %>%
+
+# # #################################################################################
+# testing data with sotomo
+# # #################################################################################
+lm3_sotomo <- lm(motions_polarization$yes_votes ~
+                   motions_polarization$ap_score_normalized +
+                   motions_polarization$lr_distance_normalized +
+                   motions_polarization$FederalCouncilProposalEncoded +
+                   motions_polarization$multiple_councils)
+summary(lm3_sotomo)
+
+# # #################################################################################
+# plots
+# # #################################################################################
+
+# # #################################################################################
+# yes_votes ~ ap_score (without sotomo)
+yesvotes_apscore <- motions_polarization %>%
   ggplot(aes(x = ap_score_normalized2, y = yes_votes, color = passed)) +
   geom_point(alpha = 0.7, size = 3) +
   geom_smooth(method = "lm", se = TRUE,
@@ -99,7 +79,69 @@ motions_polarization %>%
     color = "failed/passed"
   )
 
-motions_polarization %>%
+# save
+ggsave(filename = "img/yesvotes_apscore.png", plot = yesvotes_apscore, width = 8, height = 6, dpi = 300)
+
+# # #################################################################################
+# yes_votes ~ l/r distance (without sotomo)
+yesvotes_lrdistance <- motions_polarization %>%
+  ggplot(aes(x = lr_distance_normalized, y = yes_votes, color = passed)) +
+  geom_point(alpha = 0.7, size = 3) +
+  geom_smooth(method = "lm", se = TRUE,
+              aes(group = 1),
+              color = "black") +
+  theme_minimal() +
+  labs(
+    x = "Normalized L/R Distance",
+    y = "'Yes' votes",
+    title = "Yes Votes ~ Normalized L/R Distance",
+    color = "failed/passed"
+  )
+
+# save
+ggsave(filename = "img/yesvotes_lrdistance.png", plot = yesvotes_lrdistance, width = 8, height = 6, dpi = 300)
+
+# # #################################################################################
+# yes_votes ~ multiple councils
+yesvotes_multiplecouncils <- motions_polarization %>%
+  ggplot(aes(x = multiple_councils, y = yes_votes, color = passed)) +
+  geom_point(alpha = 0.7, size = 3) +
+  geom_smooth(method = "lm", se = TRUE,
+              aes(group = 1),
+              color = "black") +
+  theme_minimal() +
+  labs(
+    x = "Multiple Councils",
+    y = "'Yes' votes",
+    title = "Yes Votes ~ Multiple Councils",
+    color = "failed/passed"
+  )
+
+# save
+ggsave(filename = "img/yesvotes_multiplecouncils.png", plot = yesvotes_multiplecouncils, width = 8, height = 6, dpi = 300)
+
+# # #################################################################################
+# yes_votes ~ federal council proposal
+yesvotes_federalproposal <- motions_polarization %>%
+  ggplot(aes(x = FederalCouncilProposalEncoded, y = yes_votes, color = passed)) +
+  geom_point(alpha = 0.7, size = 3) +
+  geom_smooth(method = "lm", se = TRUE,
+              aes(group = 1),
+              color = "black") +
+  theme_minimal() +
+  labs(
+    x = "Federal Council Proposal",
+    y = "'Yes' votes",
+    title = "Yes Votes ~ Federal Council Proposal",
+    color = "failed/passed"
+  )
+
+# save
+ggsave(filename = "img/yesvotes_federalproposal.png", plot = yesvotes_federalproposal, width = 8, height = 6, dpi = 300)
+
+# # #################################################################################
+# lr_distance ~ ap_score (without sotomo)
+lrdistance_apscore <- motions_polarization %>%
   ggplot(aes(x = lr_distance_normalized, y = ap_score_normalized2, color = passed)) +
   geom_point(alpha = 0.7, size = 3) +
   geom_smooth(method = "lm", se = TRUE,
@@ -113,38 +155,28 @@ motions_polarization %>%
     color = "failed/passed"
   )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# save
+ggsave(filename = "img/lrdistance_apscore.png", plot = lrdistance_apscore, width = 8, height = 6, dpi = 300)
 
 
 # # #################################################################################
+# all subsets without sotomo (matrix2)
+# # #################################################################################
+
 # subset for migration
 motions_polarization_migration <- motions_polarization %>%
   filter(str_detect(TagNames, "\\bMigration\\b"))
 
-lm_migration <- lm(motions_polarization_migration$yes_votes ~ motions_polarization_migration$ap_score_normalized2)
-summary(lm_migration)
+lm_migration2 <- lm(motions_polarization_migration$yes_votes ~ 
+                      motions_polarization_migration$ap_score_normalized2 + 
+                      motions_polarization_migration$lr_distance_normalized +
+                      motions_polarization_migration$FederalCouncilProposalEncoded +
+                      motions_polarization_migration$multiple_councils)
 
-motions_polarization_migration %>%
+summary(lm_migration2)
+
+# plot
+yesvotes_apscore_migration <- motions_polarization_migration %>%
   ggplot(aes(x = ap_score_normalized2, y = yes_votes, color = passed)) +
   geom_point(alpha = 0.7, size = 3) +
   geom_smooth(method = "lm", se = TRUE,
@@ -158,23 +190,25 @@ motions_polarization_migration %>%
     color = "failed/passed"
   )
 
-lm_migration2 <- lm(motions_polarization_migration$yes_votes ~ 
-                      motions_polarization_migration$ap_score_normalized2 + 
-                      motions_polarization_migration$lr_distance_normalized +
-                      motions_polarization_migration$FederalCouncilProposalEncoded +
-                      motions_polarization_migration$multiple_councils)
+# save
+ggsave(filename = "img/yesvotes_apscore_migration.png", plot = yesvotes_apscore_migration, width = 8, height = 6, dpi = 300)
 
-summary(lm_migration2)
 
 # # #################################################################################
 # subset for traffic
 motions_polarization_traffic <- motions_polarization %>%
   filter(str_detect(TagNames, "\\bVerkehr\\b"))
 
-lm_traffic <- lm(motions_polarization_traffic$yes_votes ~ motions_polarization_traffic$ap_score_normalized2)
-summary(lm_traffic)
+lm_traffic2 <- lm(motions_polarization_traffic$yes_votes ~ 
+                    motions_polarization_traffic$ap_score_normalized2 + 
+                    motions_polarization_traffic$lr_distance_normalized +
+                    motions_polarization_traffic$FederalCouncilProposalEncoded +
+                    motions_polarization_traffic$multiple_councils)
 
-motions_polarization_traffic %>%
+summary(lm_traffic2)
+
+#plot
+yesvotes_apscore_trafic <- motions_polarization_traffic %>%
   ggplot(aes(x = ap_score_normalized2, y = yes_votes, color = passed)) +
   geom_point(alpha = 0.7, size = 3) +
   geom_smooth(method = "lm", se = TRUE,
@@ -184,27 +218,28 @@ motions_polarization_traffic %>%
   labs(
     x = "Normalized AP Score",
     y = "'Yes' votes",
-    title = "«Verkehr» - Yes Votes ~ Normalized AP Acore",
+    title = "«Traffic» - Yes Votes ~ Normalized AP Acore",
     color = "failed/passed"
   )
 
-lm_traffic2 <- lm(motions_polarization_traffic$yes_votes ~ 
-                      motions_polarization_traffic$ap_score_normalized2 + 
-                      motions_polarization_traffic$lr_distance_normalized +
-                      motions_polarization_traffic$FederalCouncilProposalEncoded +
-                      motions_polarization_traffic$multiple_councils)
-
-summary(lm_traffic2)
+# save
+ggsave(filename = "img/yesvotes_apscore_trafic.png", plot = yesvotes_apscore_trafic, width = 8, height = 6, dpi = 300)
 
 # # #################################################################################
 # subset for healthcare
 motions_polarization_health <- motions_polarization %>%
   filter(str_detect(TagNames, "\\bGesundheit\\b"))
 
-lm_health <- lm(motions_polarization_health$yes_votes ~ motions_polarization_health$ap_score_normalized2)
-summary(lm_health)
+lm_health2 <- lm(motions_polarization_health$yes_votes ~ 
+                   motions_polarization_health$ap_score_normalized2 + 
+                   motions_polarization_health$lr_distance_normalized +
+                   motions_polarization_health$FederalCouncilProposalEncoded +
+                   motions_polarization_health$multiple_councils)
 
-motions_polarization_health %>%
+summary(lm_health2)
+
+# plot
+yesvotes_apscore_healthcare <- motions_polarization_health %>%
   ggplot(aes(x = ap_score_normalized2, y = yes_votes, color = passed)) +
   geom_point(alpha = 0.7, size = 3) +
   geom_smooth(method = "lm", se = TRUE,
@@ -214,27 +249,28 @@ motions_polarization_health %>%
   labs(
     x = "Normalized AP Score",
     y = "'Yes' votes",
-    title = "«Gesundheit» - Yes Votes ~ Normalized AP Acore",
+    title = "«Healthcare» - Yes Votes ~ Normalized AP Acore",
     color = "failed/passed"
   )
 
-lm_health2 <- lm(motions_polarization_health$yes_votes ~ 
-                    motions_polarization_health$ap_score_normalized2 + 
-                    motions_polarization_health$lr_distance_normalized +
-                    motions_polarization_health$FederalCouncilProposalEncoded +
-                    motions_polarization_health$multiple_councils)
-
-summary(lm_health2)
+# save
+ggsave(filename = "img/yesvotes_apscore_healthcare.png", plot = yesvotes_apscore_healthcare, width = 8, height = 6, dpi = 300)
 
 # # #################################################################################
 # subset for science
 motions_polarization_science <- motions_polarization %>%
   filter(str_detect(TagNames, "\\bWissenschaft\\b"))
 
-lm_science <- lm(motions_polarization_science$yes_votes ~ motions_polarization_science$ap_score_normalized2)
-summary(lm_science)
+lm_science2 <- lm(motions_polarization_science$yes_votes ~ 
+                    motions_polarization_science$ap_score_normalized2 + 
+                    motions_polarization_science$lr_distance_normalized +
+                    motions_polarization_science$FederalCouncilProposalEncoded +
+                    motions_polarization_science$multiple_councils)
 
-motions_polarization_science %>%
+summary(lm_science2)
+
+# plot
+yesvotes_apscore_science <- motions_polarization_science %>%
   ggplot(aes(x = ap_score_normalized2, y = yes_votes, color = passed)) +
   geom_point(alpha = 0.7, size = 3) +
   geom_smooth(method = "lm", se = TRUE,
@@ -244,27 +280,28 @@ motions_polarization_science %>%
   labs(
     x = "Normalized AP Score",
     y = "'Yes' votes",
-    title = "«Wissenschaft» - Yes Votes ~ Normalized AP Acore",
+    title = "«Science» - Yes Votes ~ Normalized AP Acore",
     color = "failed/passed"
   )
 
-lm_science2 <- lm(motions_polarization_science$yes_votes ~ 
-                   motions_polarization_science$ap_score_normalized2 + 
-                   motions_polarization_science$lr_distance_normalized +
-                   motions_polarization_science$FederalCouncilProposalEncoded +
-                   motions_polarization_science$multiple_councils)
-
-summary(lm_science2)
+# save
+ggsave(filename = "img/yesvotes_apscore_science.png", plot = yesvotes_apscore_science, width = 8, height = 6, dpi = 300)
 
 # # #################################################################################
 # subset for europa
 motions_polarization_europa <- motions_polarization %>%
   filter(str_detect(TagNames, "\\bEuropapolitik\\b"))
 
-lm_europa <- lm(motions_polarization_europa$yes_votes ~ motions_polarization_europa$ap_score_normalized2)
-summary(lm_europa)
+lm_europa2 <- lm(motions_polarization_europa$yes_votes ~ 
+                   motions_polarization_europa$ap_score_normalized2 + 
+                   motions_polarization_europa$lr_distance_normalized +
+                   motions_polarization_europa$FederalCouncilProposalEncoded +
+                   motions_polarization_europa$multiple_councils)
 
-motions_polarization_europa %>%
+summary(lm_europa2)
+
+# plot
+yesvotes_apscore_europa <- motions_polarization_europa %>%
   ggplot(aes(x = ap_score_normalized2, y = yes_votes, color = passed)) +
   geom_point(alpha = 0.7, size = 3) +
   geom_smooth(method = "lm", se = TRUE,
@@ -278,23 +315,24 @@ motions_polarization_europa %>%
     color = "failed/passed"
   )
 
-lm_europa2 <- lm(motions_polarization_europa$yes_votes ~ 
-                    motions_polarization_europa$ap_score_normalized2 + 
-                    motions_polarization_europa$lr_distance_normalized +
-                    motions_polarization_europa$FederalCouncilProposalEncoded +
-                    motions_polarization_europa$multiple_councils)
-
-summary(lm_europa2)
+# save
+ggsave(filename = "img/yesvotes_apscore_europa.png", plot = yesvotes_apscore_europa, width = 8, height = 6, dpi = 300)
 
 # # #################################################################################
 # subset for agriculture
 motions_polarization_agri <- motions_polarization %>%
   filter(str_detect(TagNames, "\\bLandwirtschaft\\b"))
 
-lm_agri <- lm(motions_polarization_agri$yes_votes ~ motions_polarization_agri$ap_score_normalized2)
-summary(lm_agri)
+lm_agri2 <- lm(motions_polarization_agri$yes_votes ~ 
+                 motions_polarization_agri$ap_score_normalized2 + 
+                 motions_polarization_agri$lr_distance_normalized +
+                 motions_polarization_agri$FederalCouncilProposalEncoded +
+                 motions_polarization_agri$multiple_councils)
 
-motions_polarization_agri %>%
+summary(lm_agri2)
+
+# plot
+yesvotes_apscore_agriculture <- motions_polarization_agri %>%
   ggplot(aes(x = ap_score_normalized2, y = yes_votes, color = passed)) +
   geom_point(alpha = 0.7, size = 3) +
   geom_smooth(method = "lm", se = TRUE,
@@ -304,15 +342,9 @@ motions_polarization_agri %>%
   labs(
     x = "Normalized AP Score",
     y = "'Yes' votes",
-    title = "«Landwirtschaft» - Yes Votes ~ Normalized AP Acore",
+    title = "«Agriculture» - Yes Votes ~ Normalized AP Acore",
     color = "failed/passed"
   )
 
-lm_agri2 <- lm(motions_polarization_agri$yes_votes ~ 
-                   motions_polarization_agri$ap_score_normalized2 + 
-                   motions_polarization_agri$lr_distance_normalized +
-                   motions_polarization_agri$FederalCouncilProposalEncoded +
-                   motions_polarization_agri$multiple_councils)
-
-summary(lm_agri2)
-
+# save
+ggsave(filename = "img/yesvotes_apscore_agriculture.png", plot = yesvotes_apscore_agriculture, width = 8, height = 6, dpi = 300)
